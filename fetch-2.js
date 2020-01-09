a950ccc4e5c7696bbb13ce52f87dedbd3e2cd00f
PacoAudi <fsantander@comunidad.unam.mx>
2020-01-09 12:35:44 -0600

    Peticion fetch POST mando data

--- fetch-2.js
+++ fetch-2.js
@@ -1,2 +1,23 @@
-'https://reqres.in/api/users'
+//Fetch
+//POST
 
+
+let usuario ={
+    nombre: 'Paco',
+    edad: 32
+}
+
+//liga peticion, segundo argumento propiedades fetch
+fetch('https://reqres.in/api', {
+    method: 'POST', //peticion
+    body: JSON.stringify(usuario), //que queremos mandar (Json en forma de string)
+    headers:{ //Tipo de data como se esta enviando
+        'Content-Type': 'application/json'
+    }
+})
+.then(resp => resp.json() )
+.then(console.log)
+.catch(error => { //Capturo el error y lo muestro
+    console.log('Error en la peticion');
+    console.log(error);
+});
