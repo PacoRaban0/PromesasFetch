2287bf2bdcf02ca014f9f870bf15d076f0059f00
PacoAudi <fsantander@comunidad.unam.mx>
2020-01-09 11:06:33 -0600

    Peticion fetch con crossDomain

--- fetch-1.js
+++ fetch-1.js
@@ -7,3 +7,10 @@ fetch('https://reqres.in/api/users')//Respuesta puede ser cualquier cosa
         console.log(respObj.per_page);
     });
 
+//Usando cross domain
+//Para que funcione necesito extension para habilitar corss domain
+fetch('https://www.wikipedia.org')
+    .then(resp=>resp.text()) //Tratamiento a la respuesta 
+    .then(html => {
+        console.log(html);
+    });
