05ac505e00685635671ce7a1ac583f7d30266130
PacoAudi <fsantander@comunidad.unam.mx>
2020-01-09 14:00:43 -0600

    Se agrega excepcion error 404

--- fetch-4.js
+++ fetch-4.js
@@ -1,9 +1,26 @@
-fetch('https://reqres.in/api/users/1')
+fetch('https://reqres.in/api/users/1000')
     .then(resp => {
-        //Se clona la respuesta por que 
-        //una promesa no puede ser ñeida dos veces
-        resp.clone().json()
-            .then(console.log);
-        resp.json()
-            .then(console.log);
+        // //Se clona la respuesta por que 
+        // //una promesa no puede ser leida dos veces
+        // resp.clone().json()
+        //     .then(console.log);
+        // resp.json()
+        //     .then(console.log);
+
+        //Verifiva que la propidad ok de las respuesta haya sido buena
+        //Esto pasa cuando si hay respuesta pero es error 400
+        //Se hace esto por qeu el error no va a catch
+        if( resp.ok ){
+            return resp.json();
+        }else{
+            //los objetos error se crean para lanzarlos con throw
+            //Lanza una excepcion throw
+            //crea un error 
+            throw new Error('No existe el usuario 1000');
+        }
+    })
+    .then( console.log )
+    .catch(error =>{
+        console.log('Error en la peticion');
+        console.log(error);
     });
